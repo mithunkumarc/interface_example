dabf3c75c467e41a82cd1963f6bafd374030ec64
mithunkumarc <mithunchannappa@gmail.com>
2020-04-11 13:14:32 +0530

    Create multiple_inheritance_18.ts

---
+++ src/multiple_inheritance_18.ts
@@ -0,0 +1,12 @@
+//multiple inheritance in classes not allowed
+//multiple inheritance is allowed in interitance
+
+interface Animal{
+
+}
+interface Pet{
+
+}
+interface Dog extends Animal,Pet{
+
+}
